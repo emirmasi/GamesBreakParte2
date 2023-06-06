637cff52d4ff9f72a26c69d191302832a84d9e36
emirmasi <emir12345masi@gmail.com>
2023-06-06 10:48:19 -0300

    Initial commit

---
+++ app/src/main/java/com/example/GamesBreakParte2/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.GamesBreakParte2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.practicandodiseo.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

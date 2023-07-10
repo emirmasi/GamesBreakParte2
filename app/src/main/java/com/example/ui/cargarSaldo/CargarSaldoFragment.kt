292866cc0eaee681611855f892dc65bfd9ced14e
Tenshi <facku1995@gmail.com>
2023-07-09 21:43:38 -0300

    fragment cargar saldo

--- app/src/main/java/com/example/ui/cargarSaldo/CargarSaldoFragment.kt
+++ app/src/main/java/com/example/ui/cargarSaldo/CargarSaldoFragment.kt
@@ -1,5 +1,6 @@
 package com.example.ui.cargarSaldo
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,15 +8,16 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.example.practicandodiseo.R
 import com.example.practicandodiseo.databinding.FragmentCargarSaldoBinding
+import repositories.UserRepository
 
 class CargarSaldoFragment : Fragment() {
-
-    lateinit var btnCargar: Button
-    lateinit var monto: EditText
+    //var usuarioLogueado = UserRepository.usuario
     private var _binding: FragmentCargarSaldoBinding? = null
 
     // This property is only valid between onCreateView and
@@ -31,12 +33,18 @@ class CargarSaldoFragment : Fragment() {
 
         _binding = FragmentCargarSaldoBinding.inflate(inflater, container, false)
         val root: View = binding.root
-        val textView: TextView = binding.textDashboard
+        val saldo: EditText = root.findViewById(R.id.saldoinput)
+
+        val btnCargar: Button = root.findViewById(R.id.btnCargarSaldo)
+
+        btnCargar.setOnClickListener {
+            UserRepository.usuario?.cargarSaldo(saldo.text.toString().toDouble())
+            val mostrar = UserRepository.usuario?.money.toString()
+            Toast.makeText(context,mostrar,Toast.LENGTH_LONG).show()
+        }
+
         return root
-
     }
-
-
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
